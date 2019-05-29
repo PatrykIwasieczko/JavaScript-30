49b74c69ce64562f9ebd189e957304880fda44fd
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-05-29 19:08:46 +0200

    Create displayMatches function and addEventListeners

--- Ajax Type Ahead/script.js
+++ Ajax Type Ahead/script.js
@@ -13,3 +13,13 @@ function findMatches(wordToMatch, cities) {
         return place.city.match(regex) || place.state.match(regex);
     });
 }
+
+function displayMatches() {
+    console.log(this.value);
+}
+
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
