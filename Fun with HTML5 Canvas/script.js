b4a29282c8f99b3d53a63de25bc00671e2402da4
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-05-30 11:04:08 +0200

    Add drawing functionality

--- Fun with HTML5 Canvas/script.js
+++ Fun with HTML5 Canvas/script.js
@@ -5,6 +5,7 @@ canvas.width = window.innerWidth;
 ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
+ctx.lineWidth = 50;
 
 let isDrawing = false;
 let lastX = 0;
@@ -13,8 +14,20 @@ let lastY = 0;
 function draw(e) {
     if (!isDrawing) return; //stop the function from running when they are not moused down
     console.log(e);
+    ctx.beginPath();
+    // start from
+    ctx.moveTo(lastX, lastY);
+    // go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    lastX = e.offsetX;
+    lastY = e.offsetY;
 }
 canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mousedown", () => (isDrawing = true));
+canvas.addEventListener("mousedown", e => {
+    isDrawing = true;
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+});
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
