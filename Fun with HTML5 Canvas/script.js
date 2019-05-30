8b7df0547711ce4aeaa90e7238535daffa74dee3
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-05-30 10:46:56 +0200

    Add canvas styles

---
+++ Fun with HTML5 Canvas/script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
