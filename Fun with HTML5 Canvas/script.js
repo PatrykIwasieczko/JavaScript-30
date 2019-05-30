90460ff29e402268299ee2ef4733e6bd889d53a6
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-05-30 10:55:14 +0200

    Add mouse eventListeners

--- Fun with HTML5 Canvas/script.js
+++ Fun with HTML5 Canvas/script.js
@@ -5,3 +5,16 @@ canvas.width = window.innerWidth;
 ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+    if (!isDrawing) return; //stop the function from running when they are not moused down
+    console.log(e);
+}
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", () => (isDrawing = true));
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
