eccb438a196a0a94566f0ac8ca84c79d197e9cef
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-05-30 15:52:44 +0200

    Add check logic and functionality

--- Hold Shift to Check Multiple Boxes/script.js
+++ Hold Shift to Check Multiple Boxes/script.js
@@ -1,15 +1,25 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 console.log(checkboxes);
-let lastchecked;
+let lastChecked;
 
 function handleCheck(e) {
-    console.log(e);
     // Check if they had the shift key down
     // and if they are checking it
+    let inBetween = false;
     if (e.shiftKey && this.checked) {
-        // TODO
+        // loop over every checkbox
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log("Start checking");
+            }
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
     }
-    lastchecked = this;
+    lastChecked = this;
 }
 
 checkboxes.forEach(checkbox => {
