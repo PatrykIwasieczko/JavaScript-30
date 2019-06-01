f844280959fbd902b887f06ea19c6e908ac0d69c
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-06-01 18:54:00 +0200

    Strip the articles

--- Sorting Band Names Without Articles/script.js
+++ Sorting Band Names Without Articles/script.js
@@ -14,10 +14,15 @@ const bands = [
     "An Old Dog"
 ];
 
+function strip(bandName) {
+    return bandName.replace(/^(a |the |an )/i, "").trim();
+}
+
 const sortedBands = bands.sort(function(a, b) {
-    if (a > b) {
+    if (strip(a) > strip(b)) {
         return 1;
     } else {
         return -1;
     }
 });
+console.log(sortedBands);
