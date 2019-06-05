304cf013709ee52890b19f66795ca5273be65646
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-06-05 17:55:33 +0200

    Add mouse action listeners

---
+++ Click and Drag to Scroll/script.js
@@ -0,0 +1,18 @@
+const sider = document.querySelector(".items");
+let isDown = false;
+let StartX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", () => {
+    isDown = true;
+});
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+});
+slider.addEventListener("mouseup", () => {
+    isDown = false;
+});
+slider.addEventListener("mousemove", () => {
+    if (!isDown) return; // stop the function from running
+    console.count(isDown);
+});
