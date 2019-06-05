fc92b247e404a5808fbbca0d871cfaa9c1d651ac
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-06-05 22:04:29 +0200

    Add random time and hole functions

--- Whach A Mole Game/script.js
+++ Whach A Mole Game/script.js
@@ -1,3 +1,19 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+let lastHole;
+
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+    const index = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+    if (hole === lastHole) {
+        console.log("Ah nah thats the same one bud");
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
