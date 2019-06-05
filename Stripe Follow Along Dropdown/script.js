8e759ff0b0695130245317c14b7b149918654114
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-06-05 16:51:03 +0200

    Fix remove open class

--- Stripe Follow Along Dropdown/script.js
+++ Stripe Follow Along Dropdown/script.js
@@ -27,6 +27,7 @@ function handleEnter() {
 
 function handleLeave() {
     this.classList.remove("trigger-enter", "trigger-enter-active");
+    background.classList.remove("open");
 }
 
 triggers.forEach(trigger =>
